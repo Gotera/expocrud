7acb73d85f17b1c6fb76aad0ec00be5c50a6ff57
OM <orlandomedeiros.junior@gmail.com>
2022-10-28 20:55:07 -0300

    Bug on ListItem

---
+++ src/data/users.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: 1,
+        name: 'João Gomes',
+        email: 'joão.gomes@gmail.com',
+        avatarUrl:
+            'https://cdn-icons-png.flaticon.com/512/706/706839.png'
+    },
+    {
+        id: 2,
+        name: 'João Alvez',
+        email: 'joão.alvez@alvez.com',
+        avatarUrl:
+            'https://cdn-icons-png.flaticon.com/512/706/706823.png'
+    },
+    {
+        id: 3,
+        name: 'Richarles Rato',
+        email: 'richarles@gmail.com',
+        avatarUrl:
+            'https://cdn-icons-png.flaticon.com/512/706/706809.png'
+    },
+    {
+        id: 4,
+        name: 'Pop Silva',
+        email: 'pop@gmail.com',
+        avatarUrl:
+            'https://cdn-icons-png.flaticon.com/512/706/706819.png'
+    },
+];
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,31 @@
 import React from "react";
 import { Text } from "react-native"
+import { Avatar, ListItem } from "react-native-elements";
+import { FlatList, View } from "react-native-web";
+import users from "../data/users"
 
 export default props => {
+
+    function getUserItem({ item: user }) {
+        return (
+            <ListItem 
+                Avatar= {{uri: user.avatarUrl} }
+                key={user.id}
+                title={user.name}
+                subtitle={user.email}
+                bottomDivider
+                onPress={() => props.navigation.navigate('UserForm')}
+            />
+        )
+    }
+
     return (
-        <Text>UserList</Text>
+        <View>
+            <FlatList 
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            />
+        </View>
     )
 }
