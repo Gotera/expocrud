c574cc0e45ff79c31c0bdc979555e3f754500bda
OM <orlandomedeiros.junior@gmail.com>
2022-10-29 17:10:00 -0300

    ListItem Fixed

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,30 +1,66 @@
 import React from "react";
-import { Text } from "react-native"
-import { Avatar, ListItem } from "react-native-elements";
-import { FlatList, View } from "react-native-web";
+import { Alert, Text } from "react-native"
+import { Avatar, Button, Icon, ListItem } from "react-native-elements";
+import { View } from "react-native"
+import { FlatList, } from "react-native-web";
 import users from "../data/users"
 
 export default props => {
 
-    function getUserItem({ item: user }) {
+    function confirmUserDeletion(user) {
+        Alert.alert('Excluir Usuário', 'Tem certeza que deseja excluir este usuário?' [
+            {
+                text: 'Sim',
+                onPress() {
+                    console.log('delete' + user.name)
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
+
+    function getActions(user) {
+        return (
+            <>
+                <Button 
+                    onPress={() => props.novegation.navigate('UserForm', user)}
+                    type='clear'
+                    icon={<Icon name="edit" size={25} color='orange'/>}
+                />
+                <Button 
+                    onPress={() => confirmUserDeletion(user)}
+                    type='clear'
+                    icon={<Icon name="delete" size={25} color='red'/>}
+                />
+            </>
+        )
+    }
+
+    const GetUserItemTry = function getUserItem({ item: user }) {
         return (
             <ListItem 
-                Avatar= {{uri: user.avatarUrl} }
-                key={user.id}
-                title={user.name}
-                subtitle={user.email}
                 bottomDivider
                 onPress={() => props.navigation.navigate('UserForm')}
-            />
+                
+            >
+                <Avatar source={ { uri: user.avatarUrl } } />
+                <ListItem.Content>
+                    <ListItem.Title>{ user.name }</ListItem.Title>
+                    <ListItem.Subtitle>{ user.email }</ListItem.Subtitle>
+                </ListItem.Content>
+                {getActions(user)}
+            </ListItem>
         )
     }
 
     return (
         <View>
-            <FlatList 
+            <FlatList
                 keyExtractor={user => user.id.toString()}
                 data={users}
-                renderItem={getUserItem}
+                renderItem={GetUserItemTry}
             />
         </View>
     )
